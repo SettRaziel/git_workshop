d8560ffc97811a7cd1ba56e90e37edbff074eaca
Benjamin Held <me@bheld.eu>
2021-07-24 12:58:15 +0200

    add parameter and error handling

--- arguments.sh
+++ arguments.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+source ./terminal_color.sh
+
+# error handling for input parameter
+if [ "$#" -ne 1 ]; then
+  printf "%bRequired parameter missing. See gitctl.sh --help for details.%b\\n" "${RED}" "${NC}"
+  exit 1
+fi
 
 # default parameters
 ARGUMENT="DEFAULT"
 
-while [[ $# -gt 0 ]]; do
+while [[ "$#" -gt 0 ]]; do
   case ${1} in
       --init)
       ARGUMENT="INIT"; shift;;
@@ -11,7 +19,19 @@ while [[ $# -gt 0 ]]; do
       ARGUMENT="RESET"; shift;;
       --help)
       ARGUMENT="HELP"; shift;;
+      --hint)
+      ARGUMENT="HINT"; shift;;
       *)
-      shift;;
+      printf "%bUnknown parameter ${1}. See gitctl.sh --help for details.%b\\n" "${RED}" "${NC}"
+      exit 1
   esac
 done
+
+if [ ${ARGUMENT} == "HELP" ]; then
+  printf "%bscript usage:%b ./gitctl.sh parameter\\n" "${GREEN}" "${NC}"
+  printf "%bModel run parameter:\\n%b" "${YELLOW}" "${NC}"
+  printf "%b --help      %b   show help text\\n" "${LIGHT_BLUE}" "${NC}"
+  printf "%b --hint      %b   show hints for the current exercise\\n" "${LIGHT_BLUE}" "${NC}"
+  printf "%b --init      %b   initialize the exercise\\n" "${LIGHT_BLUE}" "${NC}"
+  printf "%b --reset     %b   reset the exercise\\n" "${LIGHT_BLUE}" "${NC}"
+fi
---
+++ terminal_color.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# define terminal colors
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export LIGHT_BLUE='\033[0;34m'
+export YELLOW='\033[1;33m'
+export NC='\033[0m' # No Color
